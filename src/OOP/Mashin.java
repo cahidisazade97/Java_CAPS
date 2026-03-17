061ffdbe8209411efbe8902525462dd0b0f6bce2
Cahid <cahidisazade97@gmail.com>
2026-03-17 18:58:04 +0300

    Java

---
+++ src/OOP/Mashin.java
@@ -0,0 +1,25 @@
+package OOP;
+
+public class Mashin {
+    String marka;
+    String model;
+    String rengi;
+    double motorHecmi;
+    int qapiSayi;
+
+
+    public Mashin(String marka, String model, String rengi, double motorHecmi, int qapiSayi) {
+        this.marka = marka;
+        this.model = model;
+        this.rengi = rengi;
+        this.motorHecmi = motorHecmi;
+        this.qapiSayi = qapiSayi;
+
+    }public void melumat() {
+            System.out.println("Marka: " + marka);
+            System.out.println("Model: " + model);
+            System.out.println("Rengi: " + rengi);
+            System.out.println("Motor Hecmi: " + motorHecmi);
+            System.out.println("Qapı sayı: " + qapiSayi);
+        }
+    }
---
+++ src/OOP/MashinCom.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class MashinCom {
+    static void main() {
+
+        Mashin mashin1 = new Mashin("BMW", "X5", "Qara", 3.0, 4);
+
+
+            mashin1.melumat();
+        }
+    }
+
